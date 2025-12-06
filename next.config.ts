02bf8433960a3e8596d0d6b4e15b7f12bd39adf5
quellhacked <satish2807singhniya1@gmail.com>
2025-12-06 19:57:59 +0530

    fixing the error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; font-src 'self' data:;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
